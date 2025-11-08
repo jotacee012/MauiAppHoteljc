4ae88743001b4a9dadac42afb86da8c9606fb5ba
jotacee012 <jcjoaocesar0@gmail.com>
2025-11-08 16:21:08 -0300

    mudanças para as chamadas de novas páginas em avanço

--- MauiAppHoteljc/App.xaml.cs
+++ MauiAppHoteljc/App.xaml.cs
@@ -1,13 +1,43 @@
 ﻿
+using MauiAppHoteljc.Models;
+
 namespace MauiAppHoteljc
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariAdulto = 25.0,
+                ValorDiariaCrianca = 12.5
+            }
+        };
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
---
+++ MauiAppHoteljc/Views/HospedagemContratada.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiAppHoteljc.Views;
+
+public partial class HospedagemContratada : ContentPage
+{
+	public HospedagemContratada()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+			Navigation.PopAsync();
+
+    }
+}
